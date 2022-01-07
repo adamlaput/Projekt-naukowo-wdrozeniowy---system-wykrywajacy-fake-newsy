f3e9d52cf50cb4ca6095c3cbb5fc06d034f98b12
MatPasGit <47747551+MatPasGit@users.noreply.github.com>
2022-01-07 22:25:06 +0100

    lekki format tekstu

--- front/text_extractor.js
+++ front/text_extractor.js
@@ -1,8 +1,13 @@
+
 function text_extractor() {
     var bodyScripts = document.querySelectorAll("body script");
     for(var i=0; i<bodyScripts.length; i++){
         bodyScripts[i].remove();
     }
     var str = document.body.textContent;
+    //str = str.replace(/\s/g, "");
+    str = str.replace(/(\r\n|\n|\r)/gm, "");
+    str = str.replace(/\s\s+/g, ' ');
+    //document.body.innerHTML = '<pre>'+str+'</pre>'; //do testow - odkomentowac
     return str;
   }
