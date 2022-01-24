4eb1206b7f490a51da1f089b16fd63ad3e13a005
MatPasGit <47747551+MatPasGit@users.noreply.github.com>
2022-01-24 23:57:41 +0100

    Dziala

--- front/check.js
+++ front/check.js
@@ -22,45 +22,56 @@ document.addEventListener('DOMContentLoaded', function() {
         var f = d.createElement('form');
 
         var url = 'https://fakenews-pnw-app.herokuapp.com/predict';
-        var text = document.getElementById("article").value;       
+        const text = document.getElementById("article").value;       
         var query = "text=";
         query = query.concat(text);
         url = url.concat("?", query);
         console.log(url);
-        //const Http = new XMLHttpRequest();
-        //Http.open("PUT", url, true);
-        //Http.setRequestHeader('Content-type' ,'application/json');
-        //Http.setRequestHeader('Access-Control-Allow-Origin', '*');
+        var response = "";
+        
+        const Http = new XMLHttpRequest();
+        Http.open("PUT", url, true);
+        Http.setRequestHeader('Content-type' ,'application/x-www-form-urlencoded');
+        Http.setRequestHeader('Access-Control-Allow-Origin', '*');
          
-        var response = "resp";
+        
         //function hand () {
          // console.log(this.getResponseHeader('content-type'));
         //}
         //Http.onreadystatechange = hand;
-        //Http.onreadystatechange = (e) => {
-          //console.log("siema")
-          //console.log(Http.responseText)
-          //response = Http.responseText;
-        //}
-        //Http.send(query); 
+        Http.onreadystatechange = (e) => {
+          console.log(Http.responseText)
+        }
+        
+        Http.onload  = function() {
+          response = Http.response;
+          const myObj = JSON.parse(response);
+          var x = myObj["prediction"];
+          var fake_news_probablility = 1 - x;
+          fake_news_probablility=fake_news_probablility.toFixed(2);
+          var pred = "Fake news probablility: ";
+          pred = pred.concat(fake_news_probablility);
+          var element = document.getElementById("probability");
+          element.textContent = pred;
+          
+          // do something with jsonResponse
+       };
+        Http.send(query);
+        
         //fetch(url)
         //.then(response => response.json())
         //.then(data => console.log(data));
         //console.log(response)
 
-        fetch(url, {method: 'PUT', mode: 'cors'})
+        /*fetch(url, {method: 'PUT', 
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        })
           .then(response => response.text())
-        const myObj = JSON.parse(response);
-        var x = myObj["prediction"];
-        console.log(x);
-        var fake_news_probablility = 1 - x;
-        console.log(fake_news_probablility);
-        fake_news_probablility=fake_news_probablility.toFixed(2);
-        console.log(fake_news_probablility);
-        var element = document.getElementById("probability");
-        //var element = document.querySelector("probability")
-        element.textContent = fake_news_probablility
-        
+        */
+
         
       });
     }, false);
